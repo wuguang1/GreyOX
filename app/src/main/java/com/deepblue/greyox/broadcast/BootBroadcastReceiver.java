502edbca66c2d787201d2b0b4b919cfd217a6ffc
wug <wug@deepblueai.com>
2020-09-27 15:58:33 +0800

    wg

---
+++ app/src/main/java/com/deepblue/greyox/broadcast/BootBroadcastReceiver.java
@@ -0,0 +1,24 @@
+package com.deepblue.greyox.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.deepblue.greyox.act.ActLoading;
+
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    static final String action_boot = "android.intent.action.BOOT_COMPLETED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(action_boot)) {
+            Intent mWelcomeActivity = new Intent(context, ActLoading.class);
+            // 这里必须为FLAG_ACTIVITY_NEW_TASK
+            mWelcomeActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(mWelcomeActivity);
+        }
+    }
+}
